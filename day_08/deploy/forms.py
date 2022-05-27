5b4c60ee7e2243418c20361ee48639618154a191
Jorupbek <a.jorupbek@gmail.com>
2022-05-27 22:54:00 +0300

    day08

--- day_08/deploy/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from deploy.models import Image
-
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ('title', 'file')
--- day_08/deploy/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Image(models.Model):
-    title = models.CharField(max_length=50)
-    file = models.ImageField(upload_to='pictures')
-
-    def __str__(self):
-        return self.title
